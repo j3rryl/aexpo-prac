629dc1d5ea40b7f4f8a7de047782cacb8fe6fe62
j3rryl <jeremymunroe0@gmail.com>
2024-07-24 10:12:49 +0300

    ...

--- app/(tabs)/explore/_layout.tsx
+++ app/(tabs)/explore/_layout.tsx
@@ -3,7 +3,6 @@ import { Stack } from "expo-router";
 const Page = () => {
   return (
     <Stack>
-      <Stack.Screen name="explore" options={{ headerShown: false }} />
       <Stack.Screen
         name="index"
         options={{
